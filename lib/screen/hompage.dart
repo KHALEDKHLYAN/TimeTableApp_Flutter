7831ec854d46770c7a8b1709c44061a26e2c443d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-11-24 23:26:13 +0300

    time table

--- lib/screen/hompage.dart
+++ lib/screen/hompage.dart
@@ -13,31 +13,46 @@ class HomePage extends StatelessWidget {
           actions: [
             for (var icon in [Icons.comment, Icons.settings])
               IconButton(
-                icon: Icon(icon),
-                tooltip: '${icon.toString().split('.').last} Icon',
-                onPressed: () 
-                  {}
-                
-              )
-        ],
+                  icon: Icon(icon),
+                  tooltip: '${icon.toString().split('.').last} Icon',
+                  onPressed: () {})
+          ],
           centerTitle: true,
           // leading: Icon(Icons.lock_clock_rounded),
           title: Text('TimeTable App'),
           backgroundColor: Colors.lightGreen,
         ),
         body: Center(
-          child: ElevatedButton(
-            child: const Text('Click Me!'),
-            onPressed: () {
-                Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => safeAreaBuilder(context)),
-              );
-            },),
-      ),
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 4.0,
+              mainAxisSpacing: 8.0,
+              // mainAxisExtent:
+            ),
+            scrollDirection: Axis.vertical,
+            children: [
+              Image.network('https://picsum.photos/250?image=1'),
+              Image.network('https://picsum.photos/250?image=2'),
+              Image.network('https://picsum.photos/250?image=3'),
+              Image.network('https://picsum.photos/250?image=4'),
+              Image.network('https://picsum.photos/250?image=4'),
+              Image.network('https://picsum.photos/250?image=4'),
+              Image.network('https://picsum.photos/250?image=4'),
+              Image.network('https://picsum.photos/250?image=4'),
+              Image.network('https://picsum.photos/250?image=1'),
+              Image.network('https://picsum.photos/250?image=2'),
+              Image.network('https://picsum.photos/250?image=3'),
+              Image.network('https://picsum.photos/250?image=1'),
+              Image.network('https://picsum.photos/250?image=2'),
+              Image.network('https://picsum.photos/250?image=3'),
+              Image.network('https://picsum.photos/250?image=1'),
+              Image.network('https://picsum.photos/250?image=2'),
+              Image.network('https://picsum.photos/250?image=3'),
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
-
-
